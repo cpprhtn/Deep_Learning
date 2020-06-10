aa2b20919d402aaa251c828fb049e257129d96ee
Lee_JunWon <cpprhtn@naver.com>
2020-06-10 21:40:51 +0900

    6/10

---
+++ hyperparameter_optimization.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 10 21:24:29 2020
+
+@author: cpprhtn
+"""
+
+
+'''
+하이퍼 파라미터
+    1. 하이퍼파라미터 값의 볌위를 설정
+    2. 설정된 범위에서 하이퍼파라미터의 값을 무작위로 추출
+    3. 2단계에서 샘플링한 하이퍼파라미터 값을 사용하여 학습하고, 검증 데이터로 정확도를 평가
+    4. 2단계와 3단계를 특정횟수 반봅하며, 그 정확도의 결과를 보고 하이퍼파라미터의 범위를 좁힘
+    
+    *더 세련된 기법을 원한다면 "베이즈 최적화" 참고
+'''
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
+from common.multi_layer_net import MultiLayerNet
+from common.util import shuffle_dataset
+from common.trainer import Trainer
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
+
+# 결과를 빠르게 얻기 위해 훈련 데이터를 줄임
+x_train = x_train[:500]
+t_train = t_train[:500]
+
+# 20%를 검증 데이터로 분할
+validation_rate = 0.20
+validation_num = int(x_train.shape[0] * validation_rate)
+x_train, t_train = shuffle_dataset(x_train, t_train)
+x_val = x_train[:validation_num]
+t_val = t_train[:validation_num]
+x_train = x_train[validation_num:]
+t_train = t_train[validation_num:]
+
+
+def __train(lr, weight_decay, epocs=50):
+    network = MultiLayerNet(input_size=784, hidden_size_list=[100, 100, 100, 100, 100, 100],
+                            output_size=10, weight_decay_lambda=weight_decay)
+    trainer = Trainer(network, x_train, t_train, x_val, t_val,
+                      epochs=epocs, mini_batch_size=100,
+                      optimizer='sgd', optimizer_param={'lr': lr}, verbose=False)
+    trainer.train()
+
+    return trainer.test_acc_list, trainer.train_acc_list
+
+
+# 하이퍼파라미터 무작위 탐색
+optimization_trial = 100
+results_val = {}
+results_train = {}
+for _ in range(optimization_trial):
+    # 탐색한 하이퍼파라미터의 범위 지정
+    weight_decay = 10 ** np.random.uniform(-8, -4)
+    lr = 10 ** np.random.uniform(-6, -2)
+
+    val_acc_list, train_acc_list = __train(lr, weight_decay)
+    print("val acc:" + str(val_acc_list[-1]) + " | lr:" + str(lr) + ", weight decay:" + str(weight_decay))
+    key = "lr:" + str(lr) + ", weight decay:" + str(weight_decay)
+    results_val[key] = val_acc_list
+    results_train[key] = train_acc_list
+
+print("=========== Hyper-Parameter Optimization Result ===========")
+graph_draw_num = 20
+col_num = 5
+row_num = int(np.ceil(graph_draw_num / col_num))
+i = 0
+
+for key, val_acc_list in sorted(results_val.items(), key=lambda x:x[1][-1], reverse=True):
+    print("Best-" + str(i+1) + "(val acc:" + str(val_acc_list[-1]) + ") | " + key)
+
+    plt.subplot(row_num, col_num, i+1)
+    plt.title("Best-" + str(i+1))
+    plt.ylim(0.0, 1.0)
+    if i % 5: plt.yticks([])
+    plt.xticks([])
+    x = np.arange(len(val_acc_list))
+    plt.plot(x, val_acc_list)
+    plt.plot(x, results_train[key], "--")
+    i += 1
+
+    if i >= graph_draw_num:
+        break
+
+plt.show()
